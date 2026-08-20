c022088e63b5479232eaec3083d2b478055cbf53
Muhamad Fajri <spag.959@gmail.com>
2026-08-20 11:32:25 +0700

    refactor: remove about page content and add database config to .gitignore

--- public/application/logs/log-2026-08-20.php
+++
@@ -1,7 +0,0 @@
-<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
-
-ERROR - 2026-08-20 02:49:06 --> 404 Page Not Found: Faviconico/index
-ERROR - 2026-08-20 02:57:33 --> 404 Page Not Found: Faviconico/index
-ERROR - 2026-08-20 03:48:51 --> Severity: Warning --> mysqli::real_connect(): (HY000/1045): Access denied for user 'root'@'localhost' (using password: NO) C:\laragon\www\bapenda\public\system\database\drivers\mysqli\mysqli_driver.php 211
-ERROR - 2026-08-20 03:48:51 --> Unable to connect to the database
-ERROR - 2026-08-20 03:48:51 --> 404 Page Not Found: Faviconico/index
