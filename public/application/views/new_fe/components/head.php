96d5ef2a0a479e45f2590726c2ca7ece5afd0515
Muhamad Fajri <spag.959@gmail.com>
2026-09-05 11:55:29 +0700

    feat: add new assets and update header and beranda view final mobile and desktop

--- public/application/views/new_fe/components/head.php
+++ public/application/views/new_fe/components/head.php
@@ -4,7 +4,8 @@
 <head>
     <!-- Basic -->
     <meta charset="utf-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <!-- Mobile Metas -->
+    <meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1.0, shrink-to-fit=no" />
 
     <title><?= isset($title) ? $title : 'BAPENDA' ?></title>
 
@@ -13,37 +14,19 @@
     <meta name="facebook-domain-verification" content="u7cyt5xgrow1db0yzok1uoyr02ez3i" />
 
     <!-- Favicon -->
-    <link
-        rel="shortcut icon"
-        href="<?= base_url('assets'); ?>/new/img/favicon.ico"
-        type="image/x-icon" />
+    <link rel="shortcut icon" href="<?= base_url('assets'); ?>/new/img/favicon.ico" type="image/x-icon" />
     <link rel="apple-touch-icon" href="<?= base_url('assets'); ?>/new/img/apple-touch-icon.png" />
 
-    <!-- Mobile Metas -->
-    <meta
-        name="viewport"
-        content="width=device-width, initial-scale=1, minimum-scale=1.0, shrink-to-fit=no" />
-
-    <!-- Web Fonts  -->
+    <!-- Web Fonts -->
     <link rel="preconnect" href="https://fonts.googleapis.com" />
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
-    <link
-        href="https://fonts.googleapis.com/css2?family=Molle&family=Montserrat:ital,wght@0,100..900;1,100..900&display=swap"
-        rel="stylesheet" />
-    <link
-        href="https://fonts.googleapis.com/css2?family=Goldman:wght@400;700&family=Orbitron:wght@400..900&display=swap"
-        rel="stylesheet" />
-    <link href="https://fonts.googleapis.com/css2?family=Genos:ital,wght@0,100..900;1,100..900&family=Krona+One&family=Plus+Jakarta+Sans:ital,wght@0,200..800;1,200..800&display=swap" rel="stylesheet">
-    <link
-        rel="stylesheet"
-        href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@24,200,0,0" />
+    <link href="https://fonts.googleapis.com/css2?family=Genos:ital,wght@0,100..900;1,100..900&family=Geologica:wght,CRSV@100..900,0&family=Goldman:wght@400;700&family=Krona+One&family=Molle&family=Montserrat:ital,wght@0,100..900;1,100..900&family=Open+Sans:ital,wght@0,300..800;1,300..800&family=Orbitron:wght@400..900&family=Plus+Jakarta+Sans:ital,wght@0,200..800;1,200..800&display=swap" rel="stylesheet" />
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@24,200,0,0" />
 
+    <!-- Vendor & Custom Styles -->
     <link href="<?= base_url('assets'); ?>/animation/owl.carousel.min.css" rel="stylesheet" />
     <link href="<?= base_url('assets'); ?>/animation/owl.theme.default.min.css" rel="stylesheet" />
-    <link href="<?= base_url('assets/vendor/viewerjs/viewer.min.css') ?>" rel="stylesheet">
-
-    <link
-        rel="stylesheet"
-        href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css" />
+    <link href="<?= base_url('assets/vendor/viewerjs/viewer.min.css') ?>" rel="stylesheet" />
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css" />
     <link href="<?= base_url('assets'); ?>/css/output.css" rel="stylesheet" />
 </head>
