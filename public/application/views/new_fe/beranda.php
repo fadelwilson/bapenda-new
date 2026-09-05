96ee8477ee03d5f4f3ff182930f689ed26bb1972
Muhamad Fajri <spag.959@gmail.com>
2026-09-05 10:07:28 +0700

    feat: update landing page background and refactor Tailwind CSS configuration

--- public/application/views/new_fe/beranda.php
+++ public/application/views/new_fe/beranda.php
@@ -1,7 +1,8 @@
-<?php $this->load->view('new_fe/components/head', ['title' => 'BAPENDA Purwakarta – Beranda']); ?>
+<?php $this->load->view('new_fe/components/head', ['title' => 'BAPENDA - Beranda']); ?>
 
-<body class="min-h-screen min-w-screen overflow-x-hidden relative bg-white">
+<body class="min-h-screen min-w-screen overflow-x-hidden relative" style="background-image: url('<?= base_url('assets/gambar/full-gallery-image-1.jpg') ?>'); background-size: cover; background-position: center; background-attachment: fixed;">
+    <div class="fixed inset-0 bg-gray-500/50 -z-10"></div>
+
     <?php $this->load->view('new_fe/components/beranda_sidebar', ['active_menu' => 'beranda']); ?>
-    <?php $this->load->view('new_fe/components/beranda_hero'); ?>
-    <?php $this->load->view('new_fe/components/beranda_hero_mobile'); ?>
+
 <?php $this->load->view('new_fe/components/footer_scripts'); ?>
