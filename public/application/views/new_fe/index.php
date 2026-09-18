a07aedc6bb047ae1f81cef3af9d7342fbd3f6281
fadel <fadelwilson@gmail.com>
2026-09-18 17:07:36 +0700

    Beranda

--- public/application/views/new_fe/index.php
+++ public/application/views/new_fe/index.php
@@ -24,14 +24,14 @@
 
             <div class="flex-1 flex items-center w-full">
                 <div class="w-full px-[10.992vw] max-md:px-0">
-                    <h2 class="text-(--yellow-color) text-[3.891vw] geologica leading-none max-md:text-[10.256vw] max-md:text-center">
+                    <h2 class="text-(--yellow-color) text-[3.891vw] geologica leading-none max-md:text-[10.256vw] max-md:text-justify">
                         Pembayaran Pajak Daerah Anda untuk Pembangunan Purwakarta Istimewa
                     </h2>
-                    <p class="text-[0.973vw] text-white mt-[0.584vw] open-sans max-md:text-[3.59vw] max-md:mt-[4.103vw]">
+                    <p class="text-[0.973vw] text-white mt-[0.584vw] open-sans max-md:text-[3.59vw] max-md:mt-[4.103vw] max-md:text-center">
                         Pengelola Pendapatan yang Transparant
                     </p>
 
-                    <div class="w-fit mx-auto grid grid-cols-3 max-md:grid-cols-2 gap-[1.17vw] max-md:gap-[4.103vw] mt-[2vw] max-md:mt-[8.205vw]">
+                    <div class="w-fit mx-auto grid grid-cols-3 max-md:grid-rows-1 gap-[1.17vw] max-md:gap-[4.103vw] mt-[2vw] max-md:mt-[8.205vw]">
                         <!-- Menu 1 -->
                         <div class="relative border border-white flex items-center rounded-xs justify-center bg-[#EA6D0D] p-[0.49vw] pl-[2.5vw] text-white open-sans text-[0.681vw] max-md:p-[1.538vw] max-md:pl-[9vw] max-md:text-[3.59vw]">
                             <img src="<?= base_url('assets/beranda/check-list.svg') ?>" alt="" class="absolute left-0 h-full w-[1.8vw] max-md:w-[7vw] shrink-0">
