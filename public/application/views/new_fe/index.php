9e3a4fe50d9aebfa19511f7708c2d5653f87d790
fadel <fadelwilson@gmail.com>
2026-09-18 16:49:52 +0700

    Beranda design done

--- public/application/views/new_fe/index.php
+++ public/application/views/new_fe/index.php
@@ -4,9 +4,9 @@
     <?php $this->load->view('new_fe/components/beranda_sidebar', ['active_menu' => 'beranda', 'navbar_bg' => 'blue']); ?>
 
     <!-- Section Hero / Beranda -->
-    <div class="relative min-h-screen w-full bg-cover bg-center max-md:bg-[-90px_center] overflow-hidden" style="background-image: url('<?= base_url('assets/gambar/full-gallery-image-1.jpg') ?>');">
+    <div class="relative min-h-screen w-full bg-cover bg-center max-md:bg-[-90px_center] overflow-hidden" style="background-image: url('<?= base_url('assets/beranda/new-bg.png') ?>');">
         <!-- Layer overlay biru -->
-        <div class="absolute inset-0 bg-(--blue-color)/45 z-0"></div>
+        <div class="absolute inset-0 bg-(--blue-color)/75 z-0"></div>
 
         <!-- Ornamen Sigotaka di kiri dan kanan background -->
         <img src="<?= base_url('assets/gambar/sigotaka_left.svg') ?>" alt="" class="absolute left-0 bottom-[3vw] h-[65vh] w-auto pointer-events-none select-none z-[1] opacity-30 max-md:hidden">
@@ -15,9 +15,9 @@
         <!-- Konten Header & Body -->
         <div class="relative z-10 min-h-screen flex flex-col p-[1.556vw] max-md:p-[2.051vw]">
             <div class="flex items-center justify-between max-md:flex-col max-md:items-start max-md:gap-3">
-                <img src="<?= base_url('assets/gambar/berandaico.svg') ?>" alt="Logo Bapenda" class="h-[6.229vw] w-auto object-contain max-md:w-[35vw] max-md:h-auto">
+                <img src="<?= base_url('assets/beranda/new-beranda.svg') ?>" alt="Logo Bapenda" class="h-[6.229vw] w-auto object-contain max-md:w-[35vw] max-md:h-auto">
 
-                <h1 class="text-[4.669vw] max-md:text-[12.308vw] max-md:w-full text-(--text-title) uppercase krona-one leading-none text-right">
+                <h1 class="text-[4.669vw] max-md:text-[12.308vw] max-md:w-full text-[#EA6D0D] uppercase krona-one leading-none text-right">
                     Beranda
                 </h1>
             </div>
@@ -27,33 +27,27 @@
                     <h2 class="text-(--yellow-color) text-[3.891vw] geologica leading-none max-md:text-[10.256vw] max-md:text-center">
                         Pembayaran Pajak Daerah Anda untuk Pembangunan Purwakarta Istimewa
                     </h2>
-                    <!-- <p class="text-[0.973vw] text-white mt-[0.584vw] open-sans max-md:text-[3.59vw] max-md:mt-[4.103vw]">
-                        Dinas Pendapatan Daerah Kabupaten Purwakarta hadir untuk mewujudkan tata kelola pendapatan asli daerah yang akuntabel, terbuka, dan berbasis teknologi demi pembangunan daerah yang berkeadilan.
-                    </p> -->
+                    <p class="text-[0.973vw] text-white mt-[0.584vw] open-sans max-md:text-[3.59vw] max-md:mt-[4.103vw]">
+                        Pengelola Pendapatan yang Transparant
+                    </p>
 
-                    <div class="w-fit mx-auto grid grid-cols-4 max-md:grid-cols-2 gap-[1.17vw] max-md:gap-[4.103vw] mt-[2vw] max-md:mt-[8.205vw]">
+                    <div class="w-fit mx-auto grid grid-cols-3 max-md:grid-cols-2 gap-[1.17vw] max-md:gap-[4.103vw] mt-[2vw] max-md:mt-[8.205vw]">
                         <!-- Menu 1 -->
-                        <div class="flex items-center justify-center gap-[0.29vw] bg-white p-[0.49vw] text-[#303752] open-sans text-[0.681vw] max-md:p-[1.538vw] max-md:text-[3.59vw]">
-                            <img src="<?= base_url('assets/gambar/check_circle.svg') ?>" alt="" class="size-[0.88vw] max-md:size-[4.615vw] shrink-0">
-                            <span>Transparan</span>
+                        <div class="relative border border-white flex items-center rounded-xs justify-center bg-[#EA6D0D] p-[0.49vw] pl-[2.5vw] text-white open-sans text-[0.681vw] max-md:p-[1.538vw] max-md:pl-[9vw] max-md:text-[3.59vw]">
+                            <img src="<?= base_url('assets/beranda/check-list.svg') ?>" alt="" class="absolute left-0 h-full w-[1.8vw] max-md:w-[7vw] shrink-0">
+                            <span>PBB-P2</span>
                         </div>
 
                         <!-- Menu 2 -->
-                        <div class="flex items-center justify-center gap-[0.29vw] bg-white p-[0.49vw] text-[#303752] open-sans text-[0.681vw] max-md:p-[1.538vw] max-md:text-[3.59vw]">
-                            <img src="<?= base_url('assets/gambar/check_circle.svg') ?>" alt="" class="size-[0.88vw] max-md:size-[4.615vw] shrink-0">
-                            <span>Akuntabel</span>
+                        <div class="relative border border-white flex items-center rounded-xs justify-center bg-[#EA6D0D] p-[0.49vw] pl-[2.5vw] text-white open-sans text-[0.681vw] max-md:p-[1.538vw] max-md:pl-[9vw] max-md:text-[3.59vw]">
+                            <img src="<?= base_url('assets/beranda/check-list.svg') ?>" alt="" class="absolute left-0 h-full w-[1.8vw] max-md:w-[7vw] shrink-0">
+                            <span>BPHTB</span>
                         </div>
 
                         <!-- Menu 3 -->
-                        <div class="flex items-center justify-center gap-[0.29vw] bg-white p-[0.49vw] text-[#303752] open-sans text-[0.681vw] max-md:p-[1.538vw] max-md:text-[3.59vw]">
-                            <img src="<?= base_url('assets/gambar/check_circle.svg') ?>" alt="" class="size-[0.88vw] max-md:size-[4.615vw] shrink-0">
-                            <span>Informatif</span>
-                        </div>
-
-                        <!-- Menu 4 -->
-                        <div class="flex items-center justify-center gap-[0.29vw] bg-white p-[0.49vw] text-[#303752] open-sans text-[0.681vw] max-md:p-[1.538vw] max-md:text-[3.59vw]">
-                            <img src="<?= base_url('assets/gambar/check_circle.svg') ?>" alt="" class="size-[0.88vw] max-md:size-[4.615vw] shrink-0">
-                            <span>Profesional</span>
+                        <div class="relative border border-white flex items-center rounded-xs justify-center bg-[#EA6D0D] p-[0.49vw] pl-[2.5vw] text-white open-sans text-[0.681vw] max-md:p-[1.538vw] max-md:pl-[9vw] max-md:text-[3.59vw]">
+                            <img src="<?= base_url('assets/beranda/check-list.svg') ?>" alt="" class="absolute left-0 h-full w-[1.8vw] max-md:w-[7vw] shrink-0">
+                            <span>PAD</span>
                         </div>
                     </div>
                 </div>
